e975153a22bc0036d8c0adddd60f861584b883c6
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-01-02 06:23:15 +0900

    모바일 스타일링 변경

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 started_at = Time.now - 1.day
 finished_at = Time.now + 1.month
-gift = Gift.create(name: "쿠크다스", quantity: 100, 
-                  started_at: started_at, finished_at: finished_at)
-50.times do |x|
-  user = User.create(name: "Wonsup Lee")
-  gift.winners.create(user: user)
-  puts user.created_at
-end
+gift = Gift.create(name: "쿠크다스", quantity: 100, started_at: started_at, finished_at: finished_at)
+# 50.times do |x|
+#   user = User.create(name: "Wonsup Lee")
+#   gift.winners.create(user: user)
+#   puts user.created_at
+# end
